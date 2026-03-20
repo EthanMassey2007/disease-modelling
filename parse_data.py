c9219f5cbaa9e85b370f2bdbe4dc40e61a0030f7
EthanMassey2007 <masseyethan2007@gmail.com>
2026-03-19 23:46:15 -0400

    new plans

--- parse_data.py
+++ parse_data.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import os 
+import pymc as pm
+import arviz as az
+import numpy as np
+
 
 adjacency_matrix = os.path.join(os.path.dirname(__file__),"data","adjacency_matrix_correct.parquet")
 aero_anac = os.path.join(os.path.dirname(__file__), "data", "aero_anac_2017_2023.parquet")
@@ -10,3 +14,4 @@ print(df.head())
 
 #incorporate a bayesian hierarchical model, with inputs of
 #our datatypes, as well as cases from our last project
+#need to check the granularity of data, pivot if annual
