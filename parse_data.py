5f6b01161f9f8c99bb47289f0f50e7d9d39111d1
EthanMassey2007 <masseyethan2007@gmail.com>
2026-04-16 13:51:14 -0400

    final updates for lag and tune/draw ratio

--- parse_data.py
+++ parse_data.py
@@ -626,8 +626,8 @@ with pm.Model(coords=coords) as model:
     )
 
     trace = pm.sample(
-        draws=200,
-        tune=400,
+        draws=1000,
+        tune=1500,
         chains=4,
         cores=4,
         target_accept=0.98,
